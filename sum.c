a76618e2b5e6d93efe7418120f679bdeeffb7198
madhav02032008-netizen <madhav02032008@gmail.com>
2026-02-09 12:16:35 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int s1,s2,s3,s4,s5,total;
+    printf("enter your values");
+    scanf("%d %d %d %d %d" ,&s1,&s2,&s3,&s4,&s5);
+   total=s1+s2+s3+s4+s5;
+   printf("%d",total);
+    return 0;
+}
